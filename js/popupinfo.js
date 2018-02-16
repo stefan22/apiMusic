c200e1c08efda8c42a1c179c6fce0da65961a10d
stefan22 <stefan.emailacct@gmail.com>
2018-02-16 00:51:47 +0000

    remove old popinfo.js

--- js/popupinfo.js
+++ js/popupinfo.js
@@ -1,117 +1,106 @@
-var ARTISTS = (function(a) {
+/*
+    sooo much better now ®√
+    ===========
+    - instance obj generated at artists.js
+    - removed extra constructor fn here
+    - converted prototype into a fn that handles returned event data generated 
+      at artists.js
+    - just a single img click event across ARTISTS modules
+    - enterdata now inside popup obj
+*/
 
-    var sub = a.popup = a.popup || {};
 
-    //artists
-    var alc = a.dartists;
+var ARTISTS = (function(p) {
+
+    var sub = p.popupinfo = p.popupinfo || {};
+
+    //artists module
+    var alc = p.dartists;
 
 
     var popup = {
-
         outerdiv: {
             id: "modinfo",
             class: "modal",
             type: "div"
         },
-
         outercontent: {
             class: "modal-content",
             type: "div"
         },
-
         span: {
             class: "close",
             inner: "x",
             type: "span"
         },
-
         h3: {
-            inner:"Artist Info",
+            id: alc.artist.id,
+            inner:alc.artist.name,
             type: "heading"
         },
-
         innercontent: {
             class: "modal-inner",
             inner: "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ex.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex.</p>",
             type: "div"    
-        }
+        },
+        enterdata: function(artista) {
+                artista = alc.artist || {};
+                var name,id;
+                popup.h3.inner = name = artista.name || undefined;
+                popup.h3.id = id = artista.id || undefined;
 
-    }, //popup
-
-    ArtistObj = function(name) {
-        this.name = name
-        
-
-    };
-
-    ArtistObj.prototype.enterName = function() {
-        var allartists = alc.artistsInLocalStorage();
-        for(var i=0; i < allartists.length; i++) {
-            if(allartists[i].name.indexOf(this.name) > -1) {
-                popup.h3.inner = this.name;
-                buildpopup();
-                //exit
-                i = allartists.length;
-                
-                
-            }
-        }
-        
-
-    };
+                if(name !== undefined && id !== undefined) {
+                        //showtime   
+                        buildpopup();
+                }
+        }//enterdata
+    };//popup
 
 
-
-
-
-
-    //build popup
+    //buildpopup    
     var buildpopup = function() {
         var ele = document,
             outerdiv = ele.createElement('div'),      //outerdiv
             outercontent = ele.createElement('div'),  //outercontent
             spanx = ele.createElement('span'),        //span
             h3x = ele.createElement('h3'),            //h3  
+            smx = ele.createElement('small'),
             innercontent = ele.createElement('div');  //innercontent
+        
+        //outerdiv id,class
+        outerdiv.id = popup.outerdiv.id;
+        outerdiv.className = popup.outerdiv.class;
+        //outercontent id,class
+        outercontent.className = popup.outercontent.class;
+        //span class,content
+        spanx.className = popup.span.class; 
+        spanx.textContent = popup.span.inner; 
+        //h3 content
+        h3x.textContent = popup.h3.inner;
+        //small
+        smx.textContent = "id:" + popup.h3.id;
+        
+        //innercontent class, content
+        innercontent.className = popup.innercontent.class;
+        innercontent.innerHTML = popup.innercontent.inner;
 
-            outerdiv.id = popup.outerdiv.id;
-            outerdiv.className = popup.outerdiv.class;
-        
-            outercontent.className = popup.outercontent.class;
-        
-            spanx.className = popup.span.class; 
-            spanx.textContent = popup.span.inner; 
-        
-            h3x.textContent = popup.h3.inner;
-        
-            innercontent.className = popup.innercontent.class;
-            innercontent.innerHTML = popup.innercontent.inner;
+        //append to outercontent
+        outercontent.appendChild(spanx);
+        outercontent.appendChild(h3x);
+        outercontent.appendChild(innercontent);
+        outercontent.appendChild(smx);
 
-            //append to outercontent
-            outercontent.appendChild(spanx);
-            outercontent.appendChild(h3x);
-            outercontent.appendChild(innercontent);
-            //append to outerdiv
-            outerdiv.appendChild(outercontent);
-            //append to dom
-            document.getElementById('musik').
-            appendChild(outerdiv);
+        //append to outerdiv
+        outerdiv.appendChild(outercontent);
+        //append to dom
+        document.getElementById('musik').
+        appendChild(outerdiv);
 
-            //set display
-            outerdiv.classList.add('modalOn');
+        //set display
+        outerdiv.classList.add('modalOn');
             
-
     };
 
-    
-    
-    //event fn globals
-    var modal = document.querySelector('#modinfo.modal') || undefined,  //entire modal
-        results = document.getElementById('results') || undefined,      //results 
-        modartists = document.querySelectorAll('.allartists > .artist') || undefined,
-        modclose = document.querySelector('#modinfo span.close') || undefined; //x-close
-
-    
 
 
     //outside click fn
@@ -131,20 +120,9 @@ var ARTISTS = (function(a) {
 
     };
 
-    //img click evt
-    results.addEventListener('click', function(e) {
-        //console.log(e);
-        var name = e.target.nextSibling.children[0].textContent;
-        if(e.target.tagName === "IMG" && document.getElementById('modinfo') == null) {
-            //ArtistObj(name);
-            var artone = new ArtistObj(name);
-            artone.enterName();
+   
 
-        }//if img
-
-    },false);
-
-
+    //when popup - click outside-of-it evt
     if(popup.outerdiv.id == "modinfo" || document.getElementById('modinfo')) {
         //outside click evt
         document.addEventListener('click',outsideMod, false);
@@ -153,28 +131,43 @@ var ARTISTS = (function(a) {
 
 
     
+    
+    //init img evt
+    document.getElementById('results').
+    addEventListener('click', function(e) {
+        if(e.target.tagName == "IMG") {
+            alc.getArtistsOnpage(e);
+        }
+
+        //stars at 1
+        if(alc.artist.id > 0) {
+            popup.enterdata(alc.artist);
+        }
 
 
+    },false);
+
+        
 
     
+            
+            
+           
 
-    
-
-
-
-
-
-
-
+    //public 
+    //sub.buildpopup = buildpopup; //dont need public fns,properties atm
+    //sub.enterdata = enterdata;
 
 
 
 
-    //public
-    sub.buildpopup = buildpopup;
 
 
-    return a;
 
 
-})(ARTISTS || {});
+
+
+    return p;
+
+
+})(ARTISTS || {} );
